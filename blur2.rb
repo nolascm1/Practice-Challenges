4e2eaee8c28f61c6530561ef26d8c50da5979b40
Melissa <nolascm1@gmail.com>
2019-09-06 03:44:49 +0000

    image blur2

--- blur2.rb
+++ blur2.rb
@@ -1,27 +1,39 @@
 class Image
-    def initialize (pixels)
-        @pixels = pixels
+    def initialize (array)
+        @image = array
     end
     
     def output_image 
-        @pixels.each do |subarray|
-            puts subarray.join(' ')    
+        @image.each do |subarray|
+            puts subarray.join  
         end
     end
     
-    def blur
-        @pixels.each do |subarray|
-            p subarray
-            subarray.each_with_index do |number, hindex|
-              if number == 1
-                puts "found it => #{hindex}"
-              end
-            end
-            
+    def blur!(distance=1)
+        distance.times do
+            blur_coords!
         end
     end
-end
 
+private
+
+    def blur_coords!
+        blur_coords = []
+        @image.each_with_index do |subarray, i|
+            subarray.each_with_index do |x, subarray_i|
+                blur_coords << [i, subarray_i] if x == 1
+        end
+    end
+
+    blur_coords.each do |coord|
+        @image[coord[0]][coord[1] + 1] = 1 if coord[1] + 1 <= @image[coord[0]].length - 1
+        @image[coord[0]][coord[1] - 1] = 1 if coord[1] - 1 >= 0
+        @image[coord[0] + 1][coord[1]] = 1 if coord[0] + 1 <= @image.length - 1
+        @image[coord[0] - 1][coord[1]] = 1 if coord[0] - 1 >= 0
+        end
+    end
+
+end
 image = Image.new ([
     [0,0,0,0],
     [0,1,0,0],
@@ -31,5 +43,5 @@ image = Image.new ([
 
 
 
+image.blur!(2)
 image.output_image
-image.blur
